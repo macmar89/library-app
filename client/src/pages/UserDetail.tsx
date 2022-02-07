9165bd7fc22b6bd5a3370ced99fd7375ba32c1dc
Marian <marian.karola@gmail.com>
2022-02-07 15:46:25 +0100

    FE - fetch user detail data

--- client/src/pages/UserDetail.tsx
+++ client/src/pages/UserDetail.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { UserType } from "../global/types/UserType";
 
 const UserDetail = () => {
-  return (
-    <div >
-        user detail
-    </div>
-  );
+  const { id }: { id: string } = useParams();
+  const [userDetail, setUserDetail] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      await axios
+        .get(`/api/user/${id}`)
+        .then((res) => setUserDetail(res?.data?.student))
+        .catch((err) => console.log(err));
+    };
+    fetchUser();
+  }, [id]);
+
+  return <div>user detail: {userDetail?.firstName}</div>;
 };
 
 export default UserDetail;
